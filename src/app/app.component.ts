4966b795b8c76a8ff3b2396cc3b163b2b4054d04
Patrick Koidal <pkoidal519@gmail.com>
2018-02-10 13:57:38 -0600

    update 1
    the battle of the forgotten parenthesis is finally over

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http'; 
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Weather';
+  private apiUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=4372f5664f2189869696f5af296fa35d';
+  data: any = {};
+
+  constructor(private http: Http) {
+    this.getWeather();
+    this.getData();
+
+  }
+  
+  getData() {
+    return this.http.get(this.apiUrl)
+      .map((res: Response) => res.json())
+  }
+  getWeather() {
+    this.getData().subscribe(data => {
+      console.log(data);
+      this.data = data
+    })
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
