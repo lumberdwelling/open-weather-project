50345c39bfbbf844bd18d9b7ed8baa518790d0c5
Patrick Koidal <pkoidal519@gmail.com>
2018-02-11 20:48:29 -0600

    update 5
    added service dependencies

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,18 +1,8 @@
-var zip = angular.module('zipCode', []);
+import { Injectable } from '@angular/core';
 
-zip.controller('weatherCtrl', function($scope, $http) {
+@Injectable()
+export class WeatherService {
 
-    $scope.zip = [];
-  
-    $scope.callOpenWeatherMap = function(searchZip){
-      $http.jsonp('http://api.openweathermap.org/data/2.5/find?q=' + searchZip + ',us&units=imperial&APPID=4372f5664f2189869696f5af296fa35d=&callback=JSON_CALLBACK')
-        .then(function(data){
-          console.log('success', data);
-          $scope.cities = data;
-      }).catch(function(error){
-          console.log('error', error);
-  
-      });
-    };
-  
-  });
+  constructor() { }
+
+}
