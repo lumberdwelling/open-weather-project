5a7f56744cd3a46b963bbcad4e58ca8aedbfd8ec
Patrick Koidal <pkoidal519@gmail.com>
2018-02-12 20:11:57 -0600

    Revert "update 6"
    This reverts commit 29a58642906a075e9fadbe82b4dc4fea43f97aa5.

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { AppComponent } from './app.component'
 
 @Injectable()
-export const WeatherService: zipCode = {
-  apiEndpoint: 'http://api.openweathermap.org/data/2.5/weather?zip=' + {zipCode} + ',us&units=imperial&APPID=4372f5664f2189869696f5af296fa35d'
-  title: 'Dependency'
+export class WeatherService {
 
-};
+  constructor() { }
+
+}
