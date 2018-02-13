d8037b265264ee565faa5d2209c38fb90217694f
Patrick Koidal <pkoidal519@gmail.com>
2018-02-12 20:14:29 -0600

    update 6.1
    commented out failing code to fix later

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -7,3 +7,11 @@ export class WeatherService {
   constructor() { }
 
 }
+
+/* unfinished dependency injector
+@Injectable()
+export const WeatherService: zipCode = {
+  apiEndpoint: 'http://api.openweathermap.org/data/2.5/weather?zip=' + {zipCode} + ',us&units=imperial&APPID=4372f5664f2189869696f5af296fa35d'
+  title: 'zipCode'
+
+}; */
